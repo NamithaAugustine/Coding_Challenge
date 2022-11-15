d5fb44588ac4b6f1129a5dd1557e1edf3bf574bb
Namitha Augustine <48145657+NamithaAugustine@users.noreply.github.com>
2022-11-15 17:49:28 +0530

    Add files via upload

---
+++ Q12/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Console.Write("How many entries you want? ");
+int limit = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Enter Passenger Name and Destination: ");
+
+var name = new string[limit];
+var destination = new string[limit];
+var details = new string[limit*2];
+
+for(int i = 0;i<name.Length;i++)
+{
+    Console.WriteLine();
+    Console.Write("Name: ");
+    name[i] = Console.ReadLine();
+    Console.Write("Destination: ");
+    destination[i] = Console.ReadLine();
+}
+int j = 0;
+for (int i = 0; i<limit*2;i+=2)
+{
+    
+    
+        details[i] = name[j];
+    
+        details[i+1] = destination[j];
+    j++;
+      
+}
+Console.WriteLine();
+Console.WriteLine("Details are:");
+Console.WriteLine();
+for(int i=0; i<limit*2;i++)
+{
+    Console.WriteLine(details[i]);
+}
---
+++ Q13/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] flightNumbers = { };
+bool t = true;
+string flightFare;
+int ff;
+int i =0;
+while (t)
+{
+    Console.Write("Enter Flight number or enter q to exit:");
+    flightFare = Console.ReadLine();
+    if (flightFare == "q")
+    {
+        t = false;
+        break;
+        
+    }
+    else
+    {
+        ff = int.Parse(flightFare);
+        var temp = new int[flightNumbers.Length+1];
+        for (i = 0; i < flightNumbers.Length; i++)
+        {
+            temp[i] = flightNumbers[i];
+        }
+        temp[flightNumbers.Length] = ff;
+        flightNumbers= temp;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("The Flight numbers are:");
+Console.WriteLine();
+for(i=0;i<flightNumbers.Length;i++)
+{
+    Console.WriteLine(flightNumbers[i]);
+}
