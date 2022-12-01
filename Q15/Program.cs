f4feef053203f05ff976aee8752d554771bf537f
Namitha Augustine <48145657+NamithaAugustine@users.noreply.github.com>
2022-12-01 09:45:46 +0530

    Add files via upload

---
+++ Q15/Program.cs
@@ -0,0 +1,37 @@
+﻿using Q15;
+
+Console.WriteLine("Enter your laguage weight and payment");
+
+Weight[] w= new Weight[5];
+Weight temp = new Weight();
+
+for (int i=0;i<5; i++)
+{
+    w[i]= new Weight();
+    Console.WriteLine();
+    Console.Write("Weight: ");
+    w[i].ExtraWeight= int.Parse(Console.ReadLine());
+    Console.Write("Payment: ");
+    w[i].Payment = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    Console.WriteLine($"The new payment is:{w[i].Payment}");
+}
+for(int i=0;i<4;i++)
+{
+    for(int j=0;j<4-i;j++)
+    {
+        if (w[j].ExtraWeight > w[j+1].ExtraWeight)
+        {
+            temp = w[j];
+            w[j] = w[j +1];
+            w[j +1] = temp;
+
+        }
+    }
+}
+Console.WriteLine();
+Console.Write("The sorted payment is:");
+for(int i= 0;i<5;i++)
+{
+    Console.Write(w[i].Payment+" ");
+}
---
+++ Q15/Weight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q15
+{
+    internal class Weight
+    {
+        public int ExtraWeight { get; set; }
+        private int _payment;
+        public int Payment { 
+            get { return _payment;}
+            set { 
+                _payment = value;
+                if(ExtraWeight>23)
+                    _payment += ((ExtraWeight - 23) * 15);
+            }
+        }
+    }
+}
